b3fc96336773fb45b2abe5605e10fe50c84899c4
Baranichandran S <85553313+BARANICHANDRAN@users.noreply.github.com>
2025-05-01 12:10:24 +0530

    Update settings.py

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
 ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-8371.up.railway.app',
+    'https://job-portal-nbc3.onrender.com/',
+]
 
 
 # Application definition
